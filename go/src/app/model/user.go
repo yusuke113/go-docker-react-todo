d75eefba5c7c6f36c008291756e1fa8dd9b6295d
Daichi <89190380+dchf12@users.noreply.github.com>
2023-05-08 13:37:40 +0900

    Update user.go

--- go/src/app/model/user.go
+++ go/src/app/model/user.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	ID        uint      `json:"id" gorm:"primary_key"`
-	Email     string    `json:"email" gor:"unique"`
+	Email     string    `json:"email" gorm:"unique"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
